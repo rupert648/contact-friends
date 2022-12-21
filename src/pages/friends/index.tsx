5f9c5fa832dd23212bb083cee82fd73920119103
rupert648 <rupertcarr@hotmail.co.uk>
2022-12-21 21:10:55 +0000

    fixed lint errors

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import type NextAuthPage from "../../types/NextAuthPage";
-import { trpc } from "../../utils/trpc";
 
 import AddFriendModal from "../../components/friends/AddFriendModel/AddFriendModal";
 import FriendArea from "../../components/friends/FriendArea";
@@ -11,7 +10,7 @@ const FriendsPage: NextAuthPage = () => {
   return (
     <div className="flex flex-col gap-4">
       <button
-        className="mx-10 rounded-full bg-orange-500 py-2 px-4 font-bold text-white hover:bg-orange-700"
+        className="mx-10 w-64 rounded-md border-2 border-orange-500 py-2 px-4 font-bold text-orange-800 hover:bg-orange-100"
         onClick={() => setShowModal(true)}
       >
         Add new Friend 🥹
