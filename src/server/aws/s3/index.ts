11b68228c7d1f691dc6b40076c13c5a92d8748a3
rupert648 <rupertcarr@hotmail.co.uk>
2023-01-31 12:31:41 +0000

    fix: reserved vercel env vars

--- src/server/aws/s3/index.ts
+++ src/server/aws/s3/index.ts
@@ -3,8 +3,8 @@ import AWS from "aws-sdk";
 import { env } from "../../../env/server.mjs";
 
 const s3 = new AWS.S3({
-  accessKeyId: env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: env.MY_AWS_ACCESS_KEY_ID,
+  secretAccessKey: env.MY_AWS_SECRET_ACCESS_KEY,
 });
 
 export const uploadImageBase64 = async (
